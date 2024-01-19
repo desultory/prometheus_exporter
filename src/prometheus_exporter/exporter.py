46564ff4333f5448feeabf0013ae05221c7cbf81
Zen <z@pyl.onl>
2024-01-18 18:43:04 -0600

    don't kill the app coroutine
    Signed-off-by: Zen <z@pyl.onl>

--- src/prometheus_exporter/exporter.py
+++ src/prometheus_exporter/exporter.py
@@ -49,6 +49,9 @@ class Exporter(ClassLogger):
     async def on_shutdown(self, app):
         self.logger.info("Shutting down exporter server")
         for task in all_tasks():
+            if task.get_coro().__name__ == '_run_app':
+                self.logger.debug("Skipping app task: %s", task)
+                continue
             self.logger.info("Cancelling task: %s", task)
             task.cancel()
 
