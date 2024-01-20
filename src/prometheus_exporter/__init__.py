711f089aa6325407783ecc351973ec6fdff18ae1
Zen <z@pyl.onl>
2024-01-20 12:55:52 -0600

    simplify main
    Signed-off-by: Zen <z@pyl.onl>

--- src/prometheus_exporter/__init__.py
+++ src/prometheus_exporter/__init__.py
@@ -3,4 +3,9 @@ from .cached_exporter import cached_exporter
 from .metric import Metric
 from .labels import Labels
 
-__all__ = ['Exporter', 'cached_exporter', 'Metric', 'Labels']
+DEFAULT_EXPORTER_ARGS = [
+    {'flags': ['-p', '--port'], 'dest': 'listen_port', 'type': int, 'nargs': '?', 'help': 'Port to listen on.'},
+    {'flags': ['-a', '--address'], 'dest': 'listen_ip', 'type': str, 'nargs': '?', 'help': 'Address to listen on.'},
+    {'flags': ['config_file'], 'type': str, 'nargs': '?', 'help': 'Config file to use.'}]
+
+__all__ = ['Exporter', 'cached_exporter', 'Metric', 'Labels', 'DEFAULT_EXPORTER_ARGS']
--- src/prometheus_exporter/main.py
+++ src/prometheus_exporter/main.py
@@ -1,26 +1,11 @@
-#!/usr/bin/env python3
-
-
-from zenlib.util import init_logger, init_argparser, process_args
-
+from zenlib.util import get_kwargs
 from .exporter import Exporter
+from .__init__ import DEFAULT_EXPORTER_ARGS
 
 
 def main():
-    argparser = init_argparser(prog=__package__, description='Metric Exporter for Prometheus')
-    logger = init_logger(__package__)
-
-    argparser.add_argument('-p', '--port', type=int, nargs='?', help='Port to listen on.')
-    argparser.add_argument('-a', '--address', type=str, nargs='?', help='Address to listen on.')
-
-    args = process_args(argparser, logger=logger)
-
-    kwargs = {'logger': logger}
-
-    if args.port:
-        kwargs['listen_port'] = args.port
-    if args.address:
-        kwargs['listen_ip'] = args.address
+    kwargs = get_kwargs(package=__package__, description='Metric Exporter for Prometheus',
+                        arguments=DEFAULT_EXPORTER_ARGS)
 
     exporter = Exporter(**kwargs)
     exporter.start()
