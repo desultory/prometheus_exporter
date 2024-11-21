0f2d9617a79990064853ca94a6c0189c75a346d8
Zen <z@pyl.onl>
2024-11-21 15:54:01 -0600

    add more exporter tests
    Signed-off-by: Zen <z@pyl.onl>

--- tests/test_exporter.py
+++ tests/test_exporter.py
@@ -1,3 +1,4 @@
+from asyncio import run
 from unittest import TestCase, expectedFailure, main
 from uuid import uuid4
 
@@ -6,14 +7,34 @@ from prometheus_exporter import Exporter
 from zenlib.logging import loggify
 
 
+def generate_random_metric_config(count: int) -> dict:
+    """Generate a random metric configuration"""
+    metrics = {}
+    for i in range(count):
+        name = "x" + str(uuid4()).replace("-", "_")
+        metrics[name] = {"type": "counter", "help": str(uuid4())}
+    return metrics
+
+
 class TestExporter(TestCase):
     @expectedFailure
     def test_no_config(self):
         Exporter(config_file=str(uuid4()))  # Pass a random string as config
 
     def test_proper_no_config(self):
-        """Test init with no_config_file set to True"""
-        Exporter(no_config_file=True)
+        e = Exporter(no_config_file=True)
+        self.assertIsNotNone(run(e.export()))
+
+    def test_random_metrics(self):
+        """Ensure metrics are the same run to run and properly export 100 random metrics"""
+        e = Exporter(no_config_file=True)
+        random_metrics = generate_random_metric_config(100)
+        e.config["metrics"] = random_metrics
+        export1 = run(e.export())
+        export2 = run(e.export())
+        self.assertEqual(export1, export2)
+        for metric in random_metrics:
+            self.assertIn(f"{metric} 0", export1)
 
 
 @loggify
@@ -40,6 +61,18 @@ class TestExporterAsync(AioHTTPTestCase):
             self.assertEqual(response.status, 200)
             text = await response.text()
             self.assertEqual(text, expected_response)
+
+    async def test_random_metrics(self):
+        """Ensure the server can properly serve 1000 random metrics"""
+        random_metrics = generate_random_metric_config(1000)
+        self.exporter.config["metrics"] = random_metrics
+        expected_response = await self.exporter.export()
+        async with self.client.get("/metrics") as response:
+            self.assertEqual(response.status, 200)
+            text = await response.text()
+            self.assertEqual(text, expected_response)
+        for metric in random_metrics:
+            self.assertIn(f"{metric} 0", text)
 
 
 if __name__ == "__main__":
